29d3845aa846f4ef80d3fe0fd703b3f2319ceaea
Ananya Appan <ananya.appan@gmail.com>
2018-04-14 18:59:46 +0530

    Adding Home Page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="App">
+          <p> HAVE YOU </p>
+          <p><a href = "#" class="button"> LOST </a> OR <a href = "#" class="button"> FOUND </a> </p>
+          <p> SOMETHING? </p>
+          <p><a href = "#" class="button"> See All Lost Items </a></p>
       </div>
     );
   }
